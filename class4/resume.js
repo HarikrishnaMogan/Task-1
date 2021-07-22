72ca1578da7bceb60a86be5f8774d100f0cbf55a
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-07-22 16:21:33 +0530

    Update and rename resume to resume.js

---
+++ class4/resume.js
@@ -0,0 +1,11 @@
+var resumedata = {
+    Name:"Harikrishna",
+    Degree:"Mechanical Engineering"
+    PassedYear: 2020,
+    Skills:["solidworks","Ansys"],
+    projects:["Thermoelectric Generator","3D prnting of bioamaterial PLA"],
+    Achievements:" A Comprehensive Review on Manufacturing Methods and Characterization of Al6061 and its Composites",
+    hobbies:["Badminton","BookReading"],
+    nontechnicalskills:["timemanagement","communication skills"],
+    languagesknown:["English","tamil","kannada"]                                                     
+}
