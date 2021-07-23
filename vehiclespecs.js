99910367d99e5df9a4403c5891641a1eed3d225a
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-07-23 10:31:39 +0530

    Create vehiclespecs.js

---
+++ vehiclespecs.js
@@ -0,0 +1,31 @@
+var specs = {
+    Dimensions:{overalllength:"3500 mm",
+    overallwidth:"1600 mm",
+    overallheight:"1490 mm"
+    ,wheelbase:"2360 mm",
+  trackwidthfront:"1405 mm",
+    trackwidthrear:"1400 mm"},
+
+capacities:{ seatingcapacity:5,fueltankcapacity:"35 litres"},
+
+engine:{type:"KB series",
+numberofcylindrers:3,
+numberofvalves:12,
+capacity:"998 cc/cm3",
+borexstroke:"70.3X79.5 mm",
+compressionrstio:"10:1",
+maximumpower:"67/6200 PS/rpm",maximumtorque:"09/3500 Nm/rpm"
+,fueldistripution:"multipoint injection"},
+
+transmission:"5-speed Mt",
+
+chassis:{steering:"Rack&pinion,powerassisted",
+frontbrakes:"ventilated discs",
+rearbrakes:"Drums",
+frontsuspension:"Macpherson strut& coil spring",
+rearsuspension:"Isolated trailing link & coil spring",
+shockobsorbers:"gas filled",type:"155/80R13"},
+
+weights:{kerbweight:"860-880 kg",grossvehicleweight:"1320 kg"},
+
+}
