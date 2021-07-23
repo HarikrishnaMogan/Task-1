7bfa1248c25efef68bc7614f40c9be3a671bf4c3
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-07-23 13:25:16 +0530

    Update anonymus&IIFE.js

--- class5/anonymus&IIFE.js
+++ class5/anonymus&IIFE.js
@@ -25,3 +25,25 @@ odd();
   console.log(ans);
 })();
 -------------------------------------------------------------------------------------------
+2)
+let names = ["hari","mani","kavi","subash"];
+
+let caps = function(){
+  let ans =[];
+  for(let i of names)
+  {
+    ans.push(i.toUpperCase());
+  }
+  console.log(ans);
+}
+caps();
+-----------------------------
+(function(){
+  let ans =[];
+  for(let i of names)
+  {
+    ans.push(i.toUpperCase());
+  }
+  console.log(ans);
+})();
+--------------------------------------------------------------------------------
