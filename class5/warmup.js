fa082325cd6c75747ae4ab124aeb7401bdb4a903
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-07-25 11:28:09 +0530

    Update warmup.js

--- class5/warmup.js
+++ class5/warmup.js
@@ -6,3 +6,15 @@ function addfive(num)
 var result = addfive(num);
 console.log(result);
 ----------------------------------------------
+var num= 5;
+function getopposite(num)
+{
+  if(num == parseInt(num))
+  {
+    return -num;
+  }
+  else{return -1;}
+}
+var result = getopposite(num);
+console.log(result);
+------------------------------------------------
