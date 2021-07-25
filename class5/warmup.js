708fe057e7b785a999be6974062517a8f6464794
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-07-25 12:16:28 +0530

    Update warmup.js

--- class5/warmup.js
+++ class5/warmup.js
@@ -132,3 +132,9 @@ function isSameLength(word1, word2){
  var res = isSameLength("hari","kavi");
 console.log(res);
 -------------------------------------------------------
+console.log(getDistance(100, 100, 400, 300));
+function getDistance(x1, y1, x2, y2)
+{
+  return Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2));
+}
+----------------------------------------------------------
