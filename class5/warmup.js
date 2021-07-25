130d9574c5806c3e88826ef2055aa2201b4e5e6a
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-07-25 11:34:17 +0530

    Update warmup.js

--- class5/warmup.js
+++ class5/warmup.js
@@ -26,3 +26,10 @@ function toseconds(min)
 var secs = toseconds(min);
 console.log(secs);
 ------------------------------------------------
+var mystr = "5";
+function toInteger(mystr) {
+  return parseInt(mystr);
+}
+var myint = toInteger(mystr)
+console.log(myint);
+---------------------------------------------
