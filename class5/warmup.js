4f215089485327bfa951183465b254681cfb8d3e
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-07-25 18:30:40 +0530

    Update warmup.js

--- class5/warmup.js
+++ class5/warmup.js
@@ -318,3 +318,16 @@ function reverseString(s)
   return t;
 }
 -----------------------------------------------------------------------
+var ar1 = [1, 2, 3];
+var ar2 = [4, 5, 6];
+var ar = mergeArrays(ar1, ar2);
+console.log(ar);
+function mergeArrays(ar1, ar2)
+{
+for(let el of ar2)
+ {
+ ar1.push(el);
+ }
+ return ar1;
+}
+--------------------------------------------------------------------------------
