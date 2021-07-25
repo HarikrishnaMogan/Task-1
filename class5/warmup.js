57a1910237b510f837031c674ee06bf72d590158
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-07-25 11:36:44 +0530

    Update warmup.js

--- class5/warmup.js
+++ class5/warmup.js
@@ -40,3 +40,10 @@ function nextNumber(myint) {
 var myNextint = nextNumber(myint)
 console.log(myNextint);
 -----------------------------------------------
+var arr = [1, 2, 3];
+function getFirstElement(arr) {
+  return arr[0];
+}
+var data = getFirstElement(arr)
+console.log(data);
+-----------------------------------------------
