4afbcbef97dc56eee2657eb9fa6d9c26cc305f9e
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-07-25 18:27:39 +0530

    Update warmup.js

--- class5/warmup.js
+++ class5/warmup.js
@@ -306,3 +306,15 @@ function isPrime(n)
    }
 }
 -----------------------------------------------------------------------------
+var s = reverseString("JavaScript");
+console.log(s);
+function reverseString(s)
+{
+    let t="";
+  for(let i=s.length-1;i>=0;i--)
+  {
+      t += s[i];
+  }
+  return t;
+}
+-----------------------------------------------------------------------
