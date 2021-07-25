8ce8e7d0906d1c50222b6cb473add16554636820
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-07-25 11:35:36 +0530

    Update warmup.js

--- class5/warmup.js
+++ class5/warmup.js
@@ -33,3 +33,10 @@ function toInteger(mystr) {
 var myint = toInteger(mystr)
 console.log(myint);
 ---------------------------------------------
+var myint = 0;
+function nextNumber(myint) {
+  return myint+1;
+}
+var myNextint = nextNumber(myint)
+console.log(myNextint);
+-----------------------------------------------
