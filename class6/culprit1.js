55c5ce8abf25b46b2e941fff1637ac7802f96850
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-07-26 17:14:13 +0530

    Update culprit1.js

--- class6/culprit1.js
+++ class6/culprit1.js
@@ -69,3 +69,16 @@ else
 }
   console.log(message);
 ---------------------------------------------
+let message;
+let lock = false;
+//Dont change any code below this 
+if (lock && " " || undefined )
+{
+  message = "Go away";
+}
+else
+{
+ message = "welcome";
+}
+console.log(message);
+---------------------------------------------
