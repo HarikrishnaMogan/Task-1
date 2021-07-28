42c93c219ccb67194413e4ed2139b332c903e801
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-07-28 17:10:24 +0530

    Create culprit3.js

---
+++ class6/culprit3.js
@@ -0,0 +1,12 @@
+//Fix the code to get the largest of three.
+aa = (f,s,t) => {
+    console.log(f,s,t);
+    if(f>s &&f>t){
+    console.log(f)}
+    else if(s>f && s>t){
+    console.log(s)}
+    else{
+    console.log(t)}
+   }
+   aa(1,2,3);
+-----------------------------------------------------------
