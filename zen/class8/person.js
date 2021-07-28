f92b9e5d20b521b0f817334f2079924c5ad3bf38
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-07-28 16:12:46 +0530

    Create person.js

---
+++ zen/class8/person.js
@@ -0,0 +1,13 @@
+class person
+{
+    constructor(name,age,profession,number,address)
+    {
+        this.name = name;
+        this.age = age;
+        this.profession =profession;
+        this.number=number;
+        this.address=address;
+    }
+}
+let p1 = new person("john",25,"Docter",6748932422,"xxxxxxxxxxxxxx");
+console.log(p1);
