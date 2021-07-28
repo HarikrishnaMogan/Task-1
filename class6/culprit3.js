4c0f627234110a78950651772fdfa44bc3d271f1
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-07-28 18:11:21 +0530

    Update culprit3.js

--- class6/culprit3.js
+++ class6/culprit3.js
@@ -96,3 +96,15 @@ let a =[];
     console.log(str1); 
    })("abcd");
 --------------------------------------------------------------
+//Fix the code to remove duplicates.
+var res = function(arr){
+    newArr = [];
+    for(var i=0; i < arr.length; i++){ 
+    if(newArr.indexOf(arr[i]) == -1) {
+    newArr.push(arr[i]);
+    } }
+    console.log(newArr)
+   }
+ res(["guvi","geek","guvi","duplicate","geeK"]);
+----------------------------------------------------
+  
