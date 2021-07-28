299a82e1488b13de997138742a51a750ab302e41
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-07-28 17:15:03 +0530

    Update culprit3.js

--- class6/culprit3.js
+++ class6/culprit3.js
@@ -23,3 +23,13 @@ for(var i=0;i<k.length;i++){
  return sum;
 }
 -------------------------------------------------------------
+//Fix the code to Sum of all numbers using IIFE function
+    const arr = [9,8,5,6,4,3,2,1];
+(function() {
+ let sum = 0;
+ for (var i = 0; i < arr.length; i++){
+ sum += arr[i];
+ }
+ console.log(sum);
+})();
+-----------------------------------------------------------
