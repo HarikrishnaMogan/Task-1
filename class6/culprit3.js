eefc129ce78a2e959aacb0ce895301df20ec4752
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-07-28 18:45:29 +0530

    Update culprit3.js

--- class6/culprit3.js
+++ class6/culprit3.js
@@ -148,3 +148,21 @@ var s=as.reduce(function(a,c){
  }
 console.log(s)
 -------------------------------------------------------
+//Swap the odd and even digits
+    let aa = (data)=>{
+    var a=data;
+    var l="";
+   for(i=0;i<a.length-1;i++){
+    
+    var s=a[i+1]
+    var b=a[i]
+    l+=s
+    l+=b
+    i=i+1
+   }
+   if((a.length%2)!=0){
+    l+=a[a.length-1]
+   }
+   console.log(l);
+   }
+   aa("1234");
