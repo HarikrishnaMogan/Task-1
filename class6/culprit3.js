469ea5f7294e172014b6b00cc07da4b5286d913f
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-07-28 17:25:24 +0530

    Update culprit3.js

--- class6/culprit3.js
+++ class6/culprit3.js
@@ -42,3 +42,20 @@ var ano = function(arro) {
 }
 ano(arr);
 ---------------------------------------------------------
+//Fix the code to return the Prime numbers
+    const newArray=[1,3,2,5,10];
+const myPrime=newArray.filter(num=>{
+    let count =0;
+ for(let i=2;i<=num;i++){
+ if(num%i===0)
+ {
+     count++;
+ }
+}
+if(count<=1)
+ {
+    return true;
+ }
+});
+console.log(myPrime);
+--------------------------------------------------------------------
