49c803e053558503e0bb01c5b808e0cc451cf26d
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-07-28 18:05:31 +0530

    Update culprit3.js

--- class6/culprit3.js
+++ class6/culprit3.js
@@ -82,3 +82,12 @@ let a =[];
 
  console.log(a);})();
 -----------------------------------------------------------------------
+//print all odd numbers in an array using IIFE function
+ var arr = [1, 2, 3, 5, 7, 79, 7, 2, 6, 9, 4];
+(function() {
+ for (var i = 0; i < arr.length; i++) {
+ if (arr[i] % 2 !== 0) {
+ console.log(arr[i]);
+ }}
+    ---------------------------------------------------------------
+})();
