4304942b6ecda614a9b3bf22b342aea858fff0a0
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-07-30 12:49:23 +0530

    Create population<2l.js

---
+++ zen/class9/population<2l.js
@@ -0,0 +1,20 @@
+var xhr = new XMLHttpRequest();
+xhr.open("GET","https://restcountries.eu/rest/v2/all");
+xhr.onload=function(){
+  var data = JSON.parse(this.response);
+  getrequired(data);
+}
+xhr.onerror = function()
+{
+  console.log("Error",this.statusText);
+}
+xhr.send();
+
+function getrequired(values)
+{
+   values.filter(function(v)
+   {
+    if(v.population<200000)
+     console.log(v.name);
+   })
+}
